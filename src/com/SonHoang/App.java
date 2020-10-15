a1a766f49843a18c6b6c67e3b9e65ba76d7fafa9
htsondk251 <thanh_son_hoang_70@yahoo.com>
2020-10-15 23:30:04 +0700

    Finish logIn() method
    - finish logIn() method
    - add UI for logIn() method
    - save Customers and Accounts into 2 separate lists in Bank and link
      between the lists

--- src/com/SonHoang/App.java
+++ src/com/SonHoang/App.java
@@ -12,6 +12,8 @@ public class App {
 //        System.out.println("End course project");
 
         Bank b1 = new Bank("ABC", "HN");
+
+//        b1.logIn();
         List<Account> accounts = new ArrayList<>();
         List<Customer> customers = new ArrayList<>();
         Map<String, String> userDatas = new HashMap<>();
@@ -20,14 +22,19 @@ public class App {
         Customer c2 = new Customer("172645910", "Dzung", "Hoang Thi", "16/06/1986", "HBT, HN", "dzunghoang.n21@gmail.com", "0919581286");
         Customer c3 = new Customer("172645912", "Hanh", "Tran Minh", "16/06/1997", "HBT, HN", "bighero6@gmail.com", "0344592834");
         Customer c4 = new Customer("172645913", "Minh", "Tran Nguyet", "16/06/1993", "HBT, HN", "nguyetminhbs93@gmail.com", "0335198726");
+        customers.add(c1);
+        customers.add(c2);
+        customers.add(c3);
+        customers.add(c4);
+        b1.setCustomers(customers);
 
         Account a1 = new Account(3805449019l, c1.getId());
-        accounts.add(a1);
         Account a2 = new Account(3805449020l, c2.getId());
-        accounts.add(a2);
         Account a3 = new Account(3805449021l, c3.getId());
-        accounts.add(a3);
         Account a4 = new Account(3805449022l, c4.getId());
+        accounts.add(a1);
+        accounts.add(a2);
+        accounts.add(a3);
         accounts.add(a4);
         b1.setAccounts(accounts);
 
@@ -42,10 +49,12 @@ public class App {
 //        b1.getAccounts().get(2).showDetails();
 //        b1.getAccounts().get(3).showDetails();
 
+
         b1.getAccounts().get(0).addValue(50.0);
 //        System.out.println(b1.getAccounts().get(0).getBalance());
 
-        b1.logIn();
+        b1.go();
+
     }
 
 }
--- src/com/SonHoang/Bank.java
+++ src/com/SonHoang/Bank.java
@@ -47,28 +47,136 @@ public class Bank {
         this.userDatas = userDatas;
     }
 
-    public void go() {}     //TODO: UI
+    //TODO: UI
+    public void go() {
+        System.out.println("Welcome to ABC Bank");
+        selectInHompage();
+    }
 
-    public void logIn() {
+    public void selectInHompage() {
+        int choice = 0;
+        boolean do1 = false;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            showMenuHomepage();
+            choice = verifyInputNumber(in, 2);
+            switch(choice) {
+                case 1:
+                    openAccount();
+                    break;
+                case 2:
+                    if (logIn()) {
+                        selectInAccountPage();
+                    }
+                    break;
+                case 0:
+                    do1 = true;
+                    break;
+            }
+        } while(!do1);
+    }
+
+    public void selectInAccountPage() {
+        //todo: consider initialize in only 1 methods and transfer or initialize in every methods
+//        String input="";
+        int choice;
+        boolean do1 = false;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            showMenuAccountPage();
+            choice = verifyInputNumber(in, 4);
+            switch(choice) {
+                case 1:
+                    System.out.println("Deposit money");
+                    break;
+                case 2:
+                    System.out.println("Withdraw money");
+                    break;
+                case 3:
+                    System.out.println("Close account");
+                    break;
+                case 4:
+                    System.out.println("Open saving");
+                    break;
+                case 0:
+                    do1 = true;
+                    break;
+            }
+        } while(!do1);
+    }
+
+    public boolean logIn() {
+        String email, password;
+        int failTimes = 0, input;
+        boolean success = false;
         while(true) {
-            String email, password;
             Scanner in = new Scanner(System.in);
             System.out.println("enter the email");
+            //todo: pre-process input data
             email = in.nextLine();
             System.out.println("enter the password");
             password = in.nextLine();
+
             try {
-                if (checkValidity(email, password)) {
+                if (verifyAccount(email, password)) {
                     System.out.println("login successfully");
+                    success = true;
                     break;
                 }
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
+                failTimes++;
+            }
+            if (failTimes >= 3) {
+                System.out.print("Forgot password? do you want to reset password? (y/n) ");
+                input = verifyInputYN(in);
+                if (1 == input) {
+//                    success = true;
+                    break;
+                } else {
+                    failTimes = 0;
+                }
             }
         }
+        if (success == true) {
+            Account acc = getAccount(email);
+            //todo: handle the case of account not available ??
+            if (acc != null) {
+                acc.showDetails();
+            }
+        }
+        else {
+            resetPassword();
+        }
+        return success;
     }
 
-    public boolean checkValidity(String username, String password) {
+    public Account getAccount(String email) {
+//        long accountNumber;
+        String customerId = "";
+        for (Customer c : customers) {
+            if (email.equals(c.getEmail())) {
+                customerId = c.getId();
+                break;
+            }
+        }
+        //todo: handle the case of email not available ??
+        for (Account acc : accounts) {
+            if (customerId.equals(acc.getCustomerId())) {
+                return acc;
+            }
+        }
+        return null;
+    }
+
+    public void resetPassword() {
+        //TODO: handle forgot password?
+        System.out.println("feature not available");
+    }
+
+    public boolean verifyAccount(String username, String password) {
         boolean isCompleted = false;
         //check user
         //todo: handle nullpointerexception here or outer circle
@@ -84,4 +192,77 @@ public class Bank {
         }
         return isCompleted;
     }
+
+    //main utility methods
+
+    public int verifyInputNumber(Scanner in, int caseNumber) {
+        String input;
+        int choice;
+        while(true) {
+            input = in.nextLine();
+            input = input.trim();
+            if (input.isEmpty() || (!isNumber(input)) || (isNumber(input) && ((Integer.parseInt(input)) > caseNumber) || ((Integer.parseInt(input)) < 0))) {
+                System.out.println("enter integer from 0 -> " + caseNumber);
+            } else {
+                break;
+            }
+        }
+        choice = Integer.parseInt(input);
+        return choice;
+    }
+
+    public int verifyInputYN(Scanner in) {
+        String input;
+        while(true) {
+            input = in.nextLine();
+            input = input.trim().toLowerCase();
+            if (("y".equals(input)) || ("yes".equals(input))) {
+                return 1;
+            } else if (("n".equals(input)) || ("no".equals(input))) {
+                return 0;
+            } else {
+                System.out.println("enter yes or no");
+            }
+        }
+    }
+
+    //todo: test again
+    private boolean isNumber(String s) {
+        if(s != "") {
+            for (int i = 0; i < s.length(); i++) {
+                if (Character.isDigit(s.charAt(i)) == false) return false;
+            }
+        }
+        return true;
+    }
+
+    public void showMenuHomepage() {
+        System.out.println("Please select the service");
+        System.out.println("-----------------------------------");
+        System.out.println("1. Open account");
+        System.out.println("2. Login");
+        System.out.println("0. Quit");
+        System.out.println("-----------------------------------");
+    }
+
+    public void showMenuAccountPage() {
+        System.out.println("Please select the service");
+        System.out.println("-----------------------------------");
+        System.out.println("1. Deposit money");
+        System.out.println("2. Withdraw money");
+        System.out.println("3. Close account");
+        System.out.println("4. Open saving");
+        System.out.println("0. Quit");
+        System.out.println("-----------------------------------");
+    }
+
+    public void openAccount() {
+        String input;
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the name");
+        input = in.nextLine();
+
+
+    }
 }
+
